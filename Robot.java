36b42e7360a5a63963ade80985dc725de3165a01
skebe99 <skebe7988@bronxinternationalhs.com>
2025-02-12 17:24:38 -0500

    Update Robot.java
    added more code

--- Robot.java
+++ Robot.java
@@ -1,1 +1,12 @@
+// copy right (c) FIRST and other WPILib contributions.
+// open sourse softwar; you can modify and/or share it under therms of 
+// the WPIlib BSD license file in the root direction of this project.
+
 package frc.robot;
+
+import edu.wpi.first.wpilibj.timedrobot;
+import edu.wpi.first.wpilib.xboxcontroller;
+import edu.wpi.first.wpilibj.smartdashboard.smartdashDoard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduuler;
+import frc.robot.subsystems.D
